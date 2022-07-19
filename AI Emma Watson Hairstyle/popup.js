0db62a63a00c6e37ee34f7e425e1830055459ecd
ArcInnov <arcinnovator@gmail.com>
2022-07-19 13:55:00 +0530

    Found a wig only png created htmle elemnt issue in appending #5

--- AI Emma Watson Hairstyle/popup.js
+++ AI Emma Watson Hairstyle/popup.js
@@ -25,6 +25,13 @@ function grabId() {
     let str=window.location.href;
     let id = str.slice(19);
     alert("The magic is about to happen "+id);
+    
+    const image = document.createElement('img');
+    image.setAttribute('src', 'wig-only.png');
+    image.setAttribute('alt', 'hair-wig-only');
+    const box = document.getElementsByClassName("position-relative d-inline-block col-2 col-md-12 mr-3 mr-md-0 flex-shrink-0");
+    box.appendChild(image);
+    
     //alert(str);
     //alert(str.length); // 30-11= 19 split at 19
     //alert(id);
