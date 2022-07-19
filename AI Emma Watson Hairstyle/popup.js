6be2500cb28d5a1a080c9d2ef78975db5ab00ff2
ArcInnov <arcinnovator@gmail.com>
2022-07-19 14:12:42 +0530

    trying to add image element #5

--- AI Emma Watson Hairstyle/popup.js
+++ AI Emma Watson Hairstyle/popup.js
@@ -27,10 +27,28 @@ function grabId() {
     alert("The magic is about to happen "+id);
     
     const image = document.createElement('img');
-    image.setAttribute('src', 'wig-only.png');
-    image.setAttribute('alt', 'hair-wig-only');
-    const box = document.getElementsByClassName("position-relative d-inline-block col-2 col-md-12 mr-3 mr-md-0 flex-shrink-0");
+    image.setAttribute('src', 'emma-watson.jfif');
+    const box=document.getElementById("js-pjax-container");
     box.appendChild(image);
+
+    image.onerror = function handleError() {
+        console.log('Image could not be loaded');
+        //  Can set image.src to a backup image here
+        // image.src = 'backup-image.png'
+      
+        // Or hide image
+        // image.style.display = 'none';
+      };
+      
+      image.onload = function handleImageLoaded() {
+        console.log('image loaded successfully');
+      };
+    
+   // const images=document.querySelectorAll("img");
+   // images[8].appendChild(image);
+   // console.log(images);
+    /* const box = document.getElementsByClassName(position-relative d-inline-block col-2 col-md-12 mr-3 mr-md-0 flex-shrink-0);
+    box.appendChild(image);*/
     
     //alert(str);
     //alert(str.length); // 30-11= 19 split at 19
